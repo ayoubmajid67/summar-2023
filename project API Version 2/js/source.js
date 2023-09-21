bf61abe1a8d92f755898d9bf0591bc1d0de84602
ayoubmajid67 <AYOUBMAJID71@GMAIL.COM>
2023-09-21 17:56:44 +0200

    edit

---
+++ project API Version 2/js/source.js
@@ -0,0 +1,8 @@
+const btnMode = document.getElementById("btnMode");
+const PostSide = document.getElementById("PostSide");
+
+const btnScrollDown = document.getElementById("DropDown");
+
+const UserSide = document.getElementById("UserSide");
+
+let Burger = document.getElementById("BurgerBar");
